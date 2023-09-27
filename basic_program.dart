8e233af654cf4193b7e312214441c8258343dd4c
epul2207 <epuls073@gmail.com>
2023-09-27 19:37:16 +0700

    Semangat

---
+++ basic_program.dart
@@ -0,0 +1,19 @@
+// Ini adalah program Dart sederhana yang mencetak Hello World di layar.
+//Kebanyakan programmer menulis program Hello World sebagai program pertama mereka
+void main() {
+  print('Hello World');
+
+// void main() adalah titik awal dimana eksekusi program Anda dimulai.
+// Setiap program dimulai dengan fungsi utama.
+// Tanda kurung kurawal {} menyatakan awal dan akhir suatu blok kode.
+// print("Halo Dunia!"); cetakan Halo Dunia! pada layar.
+// Setiap pernyataan kode harus diakhiri dengan titik koma.
+//contoh program untuk menampilkan name
+  var name = "Ucup";
+  print(name);
+
+// Di sini $variableName digunakan untuk menggabungkan variabel. Proses penggabungan dalam dart ini disebut interpolasi string.
+  var firstName = "Otong";
+  var lastName = "Surotong";
+  print("Full name is $firstName $lastName");
+}
